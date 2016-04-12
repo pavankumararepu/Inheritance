43da51c6a3160b3603255bb18b211020d8575672
pavankumararepu <iosdevcontributions@gmail.com>
2016-04-12 08:01:38 +0530

    Initial Commit

---
+++ Inheritance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Inheritance
+//
+//  Created by Pavankumar Arepu on 4/12/16.
+//  Copyright © 2016 PPAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
